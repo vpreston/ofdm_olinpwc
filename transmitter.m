dda78d56575aace93b48b7460fe06e9a4ead55cd
vpreston <victorialynnpreston@gmail.com>
2016-04-03 17:17:39 -0400

    creating more known data

--- transmitter.m
+++ transmitter.m
@@ -9,23 +9,33 @@
 % establish your data vector by placing the heading in the front, then
 % simply adding data
 header = create_header;
-data = create_vector(64);
+header = ifft(header)*64;
+% data = create_vector(64);
+
+known_signal = [header];
+
+for j = 1:3
+    data  = create_vector(64);
+    signal = ifft(data)*64;
+    cyclic_prefix = signal(end-15:end);
+    known_signal = [known_signal;cyclic_prefix;signal];
+end
 
 % take the IFFT
-header = ifft(header)*64;
-signal = ifft(data)*64;
+% header = ifft(header)*64;
+% signal = ifft(data)*64;
 
 % append the cyclic prefix
-cyclic_prefix = signal(end-14:end);
-appended_signal = [header;cyclic_prefix;signal];
+% cyclic_prefix = signal(end-15:end);
+% appended_signal = [header;known_signal];
 
-final_signal = [appended_signal];
+final_signal = [known_signal];
 
 % add the symbol payload of about 10-20 symbols (200 - 400 bits)
 for i = 1:15
     random_data = create_vector(64);
     random_signal = ifft(random_data)*64;
-    random_cp = random_signal(end-14:end);
+    random_cp = random_signal(end-15:end);
     true_data = [random_cp;random_signal];
     final_signal = [final_signal;true_data];
 end
